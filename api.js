5e3a59e8cbff8eded1482a4cd61afce8aa1d6bb8
Martin <martingitehi@live.com>
2018-05-07 16:47:13 +0300

    added json response

--- api.js
+++ api.js
@@ -8,11 +8,11 @@ router.post('/calculate', (req, res) => {
     let amount = Number.parseFloat(req.body.amount);
 
     if (rate < 1 && period < 1 && amount < 1) {
-        res.json({ message: 'Please provide values greater than 0' })
+        res.json({ success: false, message: 'Please provide values greater than 0' })
     }
     else {
         let monthlyRate = amortizer.computePMT(rate, period, amount);
-        return res.json(`Your query for ${amount} payable in ${period} months at ${rate}% p.a has a monthly installment of K,${monthlyRate}`)
+        return res.json({ success: true, message: `Your query for ${amount} payable in ${period} months at ${rate}% p.a has a monthly installment of K,${monthlyRate}` })
     }
 });
 
@@ -21,7 +21,12 @@ router.post('/get-schedule', (req, res) => {
     let period = Number.parseFloat(req.body.period)
     let amount = Number.parseFloat(req.body.amount);
 
-    return res.json({ schedule: amortizer.computeSchedule(amount, rate, 12, period, amortizer.computePMT(rate, period, amount)) })
+    if (rate < 1 && period < 1 && amount < 1) {
+        res.json({ success: false, message: 'Please provide values greater than 0' })
+    }
+    else {
+        return res.json({ schedule: amortizer.computeSchedule(amount, rate, 12, period, amortizer.computePMT(rate, period, amount)), success: true })
+    }
 });
 
 module.exports = router;
